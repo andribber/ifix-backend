8915a346ae741506e83326470499391ea25597de
André Ribeiro <andreemanuel2010@gmail.com>
2022-05-13 20:08:41 -0300

    Refatora projeto para se adequar ao novo escopo

--- routes/api.php
+++ routes/api.php
@@ -7,28 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/users', function() {
-    return User::all();
-});
-
-Route::get('/user/{user}', function(User $user) {
-    return ['user' => $user, 'car' => $user->cars];
-});
-
-Route::post('/user', function(Request $request) {
-
-    $attributes = [
-        'name' => $request->name,
-        'email' => $request->email,
-        'cpf' => $request->cpf,
-        'contact' => $request->contact,
-        'password' => bcrypt($request->password),
-    ];
-
-    User::create($attributes);
-
-    return response()->json([
-        'message' => 'User succesfully created'
-    ], 200);
-});
